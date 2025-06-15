01f3e839c5e42d041235a04e464d964a6b75d887
whatsacomputertho <ebalcik71@gmail.com>
2025-06-15 11:30:37 -0400

    feat(permute): optionally shift and permute schedule

--- src/cli/league/season/schedule.rs
+++ src/cli/league/season/schedule.rs
@@ -11,12 +11,21 @@ pub struct FbsimLeagueSeasonScheduleGenArgs {
     /// The number of weeks in the schedule
     #[arg(short='w')]
     #[arg(long="weeks")]
-    pub weeks: usize,
+    pub weeks: Option<usize>,
 
     /// The schedule seed
     #[arg(short='s')]
     #[arg(long="seed")]
     pub seed: Option<u64>,
+
+    /// How many places to shift the weeks of the schedule after generating it
+    #[arg(long="shift")]
+    pub shift: Option<usize>,
+
+    /// Whether to permute the schedule after generating it
+    #[arg(short='p')]
+    #[arg(long="permute")]
+    pub permute: Option<bool>,
 }
 
 /// Manage the schedule for the current season of a FootballSim league
