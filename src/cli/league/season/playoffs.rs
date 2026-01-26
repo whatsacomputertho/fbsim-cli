85bc531c59365500517f573eaf23783584da5cee
whatsacomputertho <50243356+whatsacomputertho@users.noreply.github.com>
2026-01-26 06:11:51 -0500

    feat(playoffs): implement playoff picture subcommand (#36)
    * feat(playoffs): implement playoff picture subcommand
    
    * fix(playoffs): display weeks remaining instead of games
    
    * chore(build): rebuild pointing at fbsim-core main branch

--- src/cli/league/season/playoffs.rs
+++ src/cli/league/season/playoffs.rs
@@ -41,11 +41,32 @@ pub struct FbsimLeagueSeasonPlayoffsSimArgs {
     pub league: String,
 }
 
+/// Display the playoff picture for a season
+#[derive(Args, Clone)]
+pub struct FbsimLeagueSeasonPlayoffsPictureArgs {
+    /// The input filepath for the league
+    #[arg(short='l')]
+    #[arg(long="league")]
+    pub league: String,
+
+    /// The year of the season
+    #[arg(short='y')]
+    #[arg(long="year")]
+    pub year: usize,
+
+    /// Number of playoff teams
+    #[arg(short='n')]
+    #[arg(long="num-playoff-teams")]
+    #[arg(default_value="4")]
+    pub num_playoff_teams: usize,
+}
+
 /// Manage playoffs for a season
 #[derive(Subcommand, Clone)]
 pub enum FbsimLeagueSeasonPlayoffsSubcommand {
     Gen(FbsimLeagueSeasonPlayoffsGenArgs),
     Get(FbsimLeagueSeasonPlayoffsGetArgs),
+    Picture(FbsimLeagueSeasonPlayoffsPictureArgs),
     Sim(FbsimLeagueSeasonPlayoffsSimArgs),
     Round {
         #[command(subcommand)]
